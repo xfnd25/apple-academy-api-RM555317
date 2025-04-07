47e0e9b27cb8842243e0657cfb49e7ee0c30c09a
joaocarloslima <joaocarloslima@me.com>
2025-04-07 18:54:32 -0300

    initialize apple-academy-api project with basic configuration and dependencies

---
+++ src/main/java/br/senac/sp/appleacademyapi/AppleAcademyApiApplication.java
@@ -0,0 +1,13 @@
+package br.senac.sp.appleacademyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleAcademyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleAcademyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/sp/appleacademyapi/AppleAcademyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.sp.appleacademyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleAcademyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
